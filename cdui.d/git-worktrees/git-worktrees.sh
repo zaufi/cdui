70a967a365907fff83e954ba12f36d274919cbe7
Alex Turbov <i.zaufi@gmail.com>
2026-05-31 05:34:58 +0400

    feat(git-worktree): pass entry color in the output

--- cdui.d/git-worktrees/git-worktrees.sh
+++ cdui.d/git-worktrees/git-worktrees.sh
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
+# BEGIN Default config options
 function cdui.config.plugin.git_worktrees.green()
 {
     cdui.color2ansi 'green'
@@ -13,17 +14,16 @@ function cdui.config.plugin.git_worktrees.dirty()
 {
     cdui.color2ansi 'yellow'
 }
+# END Default config options
 
 #
 # Colorize a branch name based on the cleanliness of the worktree.
 #
-# @param $1 -- branch name to render
-# @param $2 -- worktree path used to inspect git status
+# @param $1 -- worktree path used to inspect git status
 #
 function _cdui.git_worktrees.colorize_branch()
 {
-    local -r branch="${1}"
-    local -r worktree="${2}"
+    local -r worktree="${1}"
 
     local branch_color
     if [[ -z $(git -C "${worktree}" status --short 2>/dev/null) ]]; then
@@ -32,7 +32,7 @@ function _cdui.git_worktrees.colorize_branch()
         branch_color=$(cdui.config.plugin.git_worktrees.dirty)
     fi
 
-    printf '%s%s%s' "${branch_color}" "${branch}" "$(cdui.color2ansi reset)"
+    printf '%s' "${branch_color}"
 }
 
 #
@@ -43,7 +43,9 @@ function cdui.git_worktrees.get_dirs()
     local -a items=()
     while IFS=$'\t' read -r path ref; do
         items+=(
-            "$(cdui.mkentry "$(_cdui.git_worktrees.colorize_branch "${ref}" "${path}")" "${path}")"
+            "$(
+                cdui.mkentry "${ref}" "${path}" "$(_cdui.git_worktrees.colorize_branch "${path}")"
+              )"
           )
     done < <(
         git worktree list --porcelain \
@@ -55,5 +57,10 @@ function cdui.git_worktrees.get_dirs()
           '
       )
 
-    printf '%s\n' "${items[@]}"
+    if ((${#items[@]} == 0)); then
+        printf '[]\n'
+        return 0
+    fi
+
+    printf '%s\n' "${items[@]}" | jq -s add
 }
